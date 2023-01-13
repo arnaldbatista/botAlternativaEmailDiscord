23aff62204704f4719a425dc5887cd927bd5ab69
arnaldobatista <arnaldo.carpi@icloud.com>
2023-01-13 14:02:59 -0300

    feat: Integrating the Emailoutlook file in Botdiscord, thus correcting the problem of sending a single email to Discord

--- src/connections/botDiscord.js
+++ src/connections/botDiscord.js
@@ -1,89 +1,125 @@
-const fs = require('node:fs');
-const path = require('node:path');
-const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
-const { token } = require('../../config.json');
-const emailOutlook = require('./emailOutlook');
-const { setInterval } = require('node:timers');
+const { Client, Collection, Events, GatewayIntentBits } = require('discord.js')
+const mailparser = require('mailparser').simpleParser
+const Imap = require('imap')
+const fs = require('node:fs')
+const path = require('node:path')
 
+const { email, password, host, port, tls, token } = require('../../config.json');
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({ intents: [GatewayIntentBits.Guilds] })
+const imap = new Imap({
+    user: email,
+    password: password,
+    host: host,
+    port: port,
+    tls: tls
+})
 
 module.exports = function botDiscord() {
     client.once(Events.ClientReady, c => {
-        console.log(`Ready! Logged in as ${c.user.tag}`);
-    });
-    
-    client.login(token);
-    
-    client.commands = new Collection();
-    
-    function teste2() {
-        console.log('2 - chamando o teste()')
-        teste()
-    }
-    function teste() {
-        console.log('3 - inicio do teste()')
+        console.log(`Ready! Logged in as ${c.user.tag}`)
+    })
 
+    client.login(token)
 
-        emailOutlook().then(res => {
-            console.log('inicio do res =>')
-            // console.log(res)
-            // client.channels.cache.get('1058449545488506982').send(res.link)
-            let teste = []
-            teste.push(res)
-    
-            setInterval(() => {
-                if(teste.length !== 0){
-                    console.log(teste)
-                    teste.splice(0)
-                    console.log('dentro do loop')
-                    clearInterval()
-                    console.log('clearInterval()')
-                    teste2()
-                    console.log('dps de chamar o teste2')
-                }
-            }, 1000) 
-            console.log('final do res =>')
-            return           
-        })
+    imap.connect()
 
-        
-        console.log('4 - final do teste()')
-    }
-    console.log('1 - chamando o teste2')
-    teste2()
+    imap.on('error', err => {
+        console.log(err)
+        imap.connect()
+    })
 
+    client.commands = new Collection()
 
-
-    const commandsPath = path.join(__dirname, '..', 'commands');
-    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+    const commandsPath = path.join(__dirname, '..', 'commands')
+    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'))
 
     for (const file of commandFiles) {
-        const filePath = path.join(commandsPath, file);
-        const command = require(filePath);
-        // Set a new item in the Collection with the key as the command name and the value as the exported module
+        const filePath = path.join(commandsPath, file)
+        const command = require(filePath)
         if ('data' in command && 'execute' in command) {
-            client.commands.set(command.data.name, command);
+            client.commands.set(command.data.name, command)
         } else {
-            console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+            console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`)
         }
     }
 
     client.on(Events.InteractionCreate, async interaction => {
-        if (!interaction.isChatInputCommand()) return;
+        if (!interaction.isChatInputCommand()) return
 
-        const command = interaction.client.commands.get(interaction.commandName);
+        const command = interaction.client.commands.get(interaction.commandName)
 
         if (!command) {
-            console.error(`No command matching ${interaction.commandName} was found.`);
-            return;
+            console.error(`No command matching ${interaction.commandName} was found.`)
+            return
         }
 
         try {
-            await command.execute(interaction);
+            await command.execute(interaction)
         } catch (error) {
-            console.error(error);
-            await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+            console.error(error)
+            await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true })
         }
-    });
+    })
+
+    imap.once('ready', function () {
+        imap.openBox('INBOX', true, (err, box) => {
+            if (err) throw err
+            imap.on('mail', (numNewMsgs) => {
+                console.log(numNewMsgs + ' new message(s)')
+
+                fetchEmails().then((result) => {
+                    client.channels.cache.get('1058449545488506982').send(result)
+                });
+
+                function fetchEmails() {
+                    return new Promise((resolve, reject) => {
+                        imap.search(['UNSEEN'], (err, results) => {
+                            const checkEmail = imap.fetch(results, { bodies: '' })
+
+                            checkEmail.on('message', (msg, seqno) => {
+                                msg.on('body', (stream, info) => {
+                                    mailparser(stream)
+                                        .then(email => {
+                                            const linkEmail = JSON.stringify(
+                                                JSON.stringify(
+                                                    JSON.stringify(email.html).split('"')
+                                                ).split("\\")
+                                            ).split('"')
+
+                                            function filterItens(filterByHttps) {
+                                                return linkEmail.filter(el => {
+                                                    return el.toLocaleLowerCase().indexOf(filterByHttps.toLocaleLowerCase()) > -1
+                                                })
+                                            }
+
+                                            resolve(filterItens('https')[2])
+                                        })
+                                })
+                            })
+                        })
+                    })
+                }
+            })
+        })
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
